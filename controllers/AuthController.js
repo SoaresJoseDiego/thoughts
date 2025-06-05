6fb0782b1811a1778dffc2ae49a5e2c5e521a0ae
Diego Soares <dsadamovich@gmail.com>
2025-06-05 12:45:51 -0300

    aula

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,102 +1,89 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
+module.exports = class AuthController {
+  static login(req, res) {
+    res.render("auth/login");
+  }
 
-module.exports = class AuthController{f
-    static login(req, res){
-        res.render('auth/login')
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/login");
+      return;
+    }
+    // check if password match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+      return;
     }
 
-    static async loginPost(req, res){
-        const {email, password} = req.body
+    req.session.userid = user.id;
 
-        // find user
-        const user = await User.findOne({where: {email: email}})
+    req.flash("message", "Logado com sucesso!");
 
-        if(!user){
-            req.flash('message', 'Usuário não encontrado!')
-            res.render('auth/login')
-            return
-        }
-    
-        // check if password match
-        const passwordMatch = bcrypt.compareSync(password, user.password);
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
 
-        if(!passwordMatch){
-            req.flash('message', 'Senha inválida!')
-            res.render('auth/login')
-            return;
-        }
+  static register(req, res) {
+    res.render("auth/register");
+  }
 
-        req.session.userid = user.id
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
 
-        req.flash('message', 'Logado com sucesso!')
+    // passwords match validation
+    if (password != confirmpassword) {
+      req.flash("message", "As senhas não conferem, tente novamente!");
+      res.render("auth/register");
 
-        req.session.save(() => {
-            res.redirect('/')
-        })
+      return;
+    }
+    // check if user exists
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
 
+    if (checkIfUserExists) {
+      req.flash("message", "O e-mail já está em uso!");
+      res.render("auth/register");
     }
 
-    static register(req, res){
-        res.render('auth/register')
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+
+      //initialize session
+
+      req.session.userid = createdUser.id;
+
+      req.flash("message", "Usuário cadastrado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
     }
-
-
-
-    static async registerPost(req, res){
-
-        const { name, email, password, confirmpassword } = req.body
-
-        // passwords match validation
-        if (password != confirmpassword) {
-          req.flash('message', 'As senhas não conferem, tente novamente!')
-          res.render('auth/register')
-    
-          return
-        }
-        // check if user exists
-        const checkIfUserExists = await User.findOne({where: {email: email}})
-
-        if(checkIfUserExists){
-            req.flash('message', 'O e-mail já está em uso!')
-            res.render('auth/register')
-        }
-
-        const salt = bcrypt.genSaltSync(10)
-        const hashedPassword = bcrypt.hashSync(password, salt)
-
-        const user = {
-            name, email, password: hashedPassword
-        }
-
-        try{
-           const createdUser = await User.create(user)
-
-                //initialize session
-
-                req.session.userid = createdUser.id
-
-
-                req.flash('message', 'Usuário cadastrado com sucesso!')
-
-                req.session.save(() => {
-                    res.redirect('/')
-                })
-
-        }catch(err){
-            console.log(err);
-        }
-
-    }
-
-
-    static logout(req,res){
-        req.session.destroy()
-        res.redirect('/login')
-    }
-
-
-}
-
+  }
+  static logout(req, res) {
+    req.session.destroy();
+    res.redirect("/login");
+  }
+};
